8ce07caa17a2dcf3fbf18022f12dcd8c40b4ff0b
YosephAhmed <yosephahmed@aol.com>
2020-06-03 00:43:41 -0400

    Initialized the React app and Rendered App component

--- src/index.js
+++ src/index.js
@@ -3,3 +3,10 @@
 // Create the <App /> component from scratch
 // Have the <App /> component render 3 or 4 checkboxes with paragraphs or spans next to it
 // like you're making a todo list with some hard-codes items on it
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"))
