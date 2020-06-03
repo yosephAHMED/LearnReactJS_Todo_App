cd027b8d9d3f36ac0db180c54269e95049af3b89
YosephAhmed <yosephahmed@aol.com>
2020-06-03 12:38:59 -0400

    Moved checkboxes to seperate component file. Fixed component call in App.js. Updated tasks/prompts in index.js.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
-// Have the <App /> component render 3 or 4 checkboxes with paragraphs or spans next to it
-// like you're making a todo list with some hard-codes items on it
+import TodoItem from './components/TodoItem';
 
 function App() {
     let checkbox = document.createElement("INPUT");
     checkbox.setAttribute("type", "checkbox");
     return(
         <div>
-            <input type="checkbox" />
-            <p>Placeholder text here</p>
+            <TodoItem />
 
-            <input type="checkbox" />
-            <p>Placeholder text here</p>
+            <TodoItem />
 
-            <input type="checkbox" />
-            <p>Placeholder text here</p>
+            <TodoItem />
 
-            <input type="checkbox" />
-            <p>Placeholder text here</p>
+            <TodoItem />
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+// PART ONE - COMPLETED
 // From scratch, initialize the React app
 // Render an <App /> component
 // Create the <App /> component from scratch
 // Have the <App /> component render 3 or 4 checkboxes with paragraphs or spans next to it
 // like you're making a todo list with some hard-codes items on it
+
+// PART TWO - IN PROGRESS
+// 1. Change the input/p combo to be a new component called <TodoItem />.
+// For now, this new component will just have the same code as what it is in the App component
+// 2. Style the page
 
 import React from 'react';
 import ReactDOM from 'react-dom';
